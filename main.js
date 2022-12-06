0e62d54ea16488b5d836ea2fb946275d20b25176
jmir1 <jhmiramon@gmail.com>
2022-12-06 19:03:47 +0100

    use new octocat

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
 const core = require('@actions/core');
-const { Octokit } = require("@octokit/rest");
+const github = require('@actions/github');
 
 const repository = core.getInput('repository');
 var owner = core.getInput('owner');
 var repo = core.getInput('repo');
 var excludes = core.getInput('excludes').trim().split(",");
 
-const octokit = new Octokit()
+const octokit = github.getOctokit(myToken);
 
 async function run() {
     try {
         if (repository){
                 [owner, repo] = repository.split("/");
         }
-        var releases  = await octokit.repos.listReleases({
+        var releases = (await octokit.rest.repos.listReleases({
             owner: owner,
             repo: repo,
-            });
-        releases = releases.data;
+            })).data;
         if (excludes.includes('prerelease')) {
             releases = releases.filter(x => x.prerelease != true);
         }
         if (excludes.includes('draft')) {
-            releases = releases.filter(x => x.draft != true);
+            releases = releases.data.filter(x => x.draft != true);
         }
         if (releases.length) {
             core.setOutput('release', releases[0].tag_name);
